95f1f4ba3b404487d54f200ba094af1f75502057
Beniez-paola <124212478+Beniez-paola@users.noreply.github.com>
2023-02-16 14:39:31 -0800

    Update hola.c

--- code/hola.c
+++ code/hola.c
@@ -1,12 +1,16 @@
 
+###Comentarios del codigo fuente
+
+
+Codigo **Fuente**
+
 ```javascript
-#public class HolaMundo {
+public class HolaMundo {
 
 	public static void main(String[] args) {		
 		System.out.println("Hola Mundo");
 	}
 
 }
-
 
 ```
